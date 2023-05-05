7c68c578d072109b1b26715c5e42e57eb39f1fde
JonathanTheZero <jonathan.frohne@web.de>
2023-05-05 12:57:37 +0200

    Fixed unclosed file leak

--- U3_IO/songtext.hs
+++ U3_IO/songtext.hs
@@ -5,29 +5,31 @@ import System.IO
 {-
 readFileContent ::String
 readFileContent = do
-    rawText <- openFile "one-hit-wonder.txt" ReadMode
-    inhalt <- hGetContents rawText
+    file <- openFile "one-hit-wonder.txt" ReadMode
+    inhalt <- hGetContents file
     inhalt
 -}
 
 printText :: IO ()
 printText = do
-    rawText <- openFile "one-hit-wonder.txt" ReadMode
-    inhalt <- hGetContents rawText
+    file <- openFile "one-hit-wonder.txt" ReadMode
+    inhalt <- hGetContents file
     putStrLn inhalt
+    hClose file
 
 printTextInStyle :: IO ()
 printTextInStyle = do
     putStrLn "In Welchem Stil soll der Text geschrieben werden?"
     stil <- getLine
-    rawText <- openFile "one-hit-wonder.txt" ReadMode
-    inhalt <- hGetContents rawText
+    file <- openFile "one-hit-wonder.txt" ReadMode
+    inhalt <- hGetContents file
     if stil == "laut"
         then putStrLn [toUpper x | x <- inhalt]
         else
             if stil == "leise"
                 then putStrLn [toLower x | x <- inhalt]
                 else putStrLn inhalt
+    hClose file
 
 printTextWithName :: IO ()
 printTextWithName = do
@@ -35,8 +37,8 @@ printTextWithName = do
     stil <- getLine
     putStrLn "Was ist dein Name?"
     name <- getLine
-    rawText <- openFile "one-hit-wonder.txt" ReadMode
-    inhalt <- hGetContents rawText
+    file <- openFile "one-hit-wonder.txt" ReadMode
+    inhalt <- hGetContents file
     let inhaltWName :: String = unlines [unwords [if z == "Macarena" then name else z | z <- words x] | x <- lines inhalt]
     if stil == "laut"
         then putStrLn [toUpper x | x <- inhaltWName]
@@ -44,3 +46,4 @@ printTextWithName = do
             if stil == "leise"
                 then putStrLn [toLower x | x <- inhaltWName]
                 else putStrLn inhaltWName
+    hClose file
