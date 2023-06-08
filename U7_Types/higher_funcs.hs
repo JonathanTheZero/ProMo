e15aac7e4a84245931eef02c5d4791b1f9c63cf0
JonathanTheZero <jonathan.frohne@web.de>
2023-06-08 16:49:48 +0200

    Finsihed U7

--- U7_Types/higher_funcs.hs
+++ U7_Types/higher_funcs.hs
@@ -10,4 +10,25 @@ map' f xs = [f x | x <- xs]
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ _ [] = []
 zipWith' _ [] _ = []
-zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+zipWith' f (x : xs) (y : ys) = f x y : zipWith' f xs ys
+
+unzipWith :: (t -> (a, b)) -> [t] -> ([a], [b])
+unzipWith f [] = ([], [])
+unzipWith f xs = (unzipA f xs, unzipB f xs)
+  where
+    unzipA :: (t -> (a, b)) -> [t] -> [a]
+    unzipA f [] = []
+    unzipA f (x : xs) = fst (f x) : unzipA f xs
+    unzipB :: (t -> (a, b)) -> [t] -> [b]
+    unzipB f [] = []
+    unzipB f (x : xs) = snd (f x) : unzipB f xs
+
+-- and now the more beautiful way, thanks ChatGPT
+unzipWith' :: (t -> (a, b)) -> [t] -> ([a], [b])
+unzipWith' f [] = ([], [])
+unzipWith' f (x : xs) =
+    let
+        (a, b) = f x
+        (as, bs) = unzipWith' f xs
+     in
+        (a : as, b : bs)
