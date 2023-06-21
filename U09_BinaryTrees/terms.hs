07220a8d96e84a465570222246e48f8620dad667
Jonathan Frohne <jonathan.frohne@web.de>
2023-06-21 13:51:10 +0200

    Finished U9

---
+++ U09_BinaryTrees/terms.hs
@@ -0,0 +1,37 @@
+data Operation = Times | Plus | Minus | Division deriving (Show)
+data Sign = Positive | Negate deriving (Show)
+data Term a = C a | BinaryTerm Operation (Term a) (Term a) | UnaryTerm Sign (Term a) deriving (Show)
+
+x :: Fractional a => Term a
+x =
+    BinaryTerm
+        Times
+        (BinaryTerm Plus (C 5) (C 4))
+        (BinaryTerm Minus (C 3) (C 2))
+
+operator :: Fractional a => Operation -> a -> a -> a
+operator Times = (*)
+operator Plus = (+)
+operator Minus = (-)
+operator Division = (/)
+
+eval :: Fractional a => Term a -> a
+eval (C x) = x
+eval (UnaryTerm Negate t) = (-1) * eval t
+eval (BinaryTerm op a b) = (operator op) (eval a) (eval b)
+
+y :: Fractional a => Term a
+y =
+    BinaryTerm
+        Times
+        (BinaryTerm Plus (C 5) (UnaryTerm Negate (C 4)))
+        (BinaryTerm Minus (C 3) (C 2))
+
+simplify :: Term a -> Term a
+simplify (UnaryTerm Negate (UnaryTerm Negate t)) = t
+simplify (BinaryTerm Plus t1 (UnaryTerm Negate t2)) = BinaryTerm Minus t1 t2
+simplify (BinaryTerm Minus t1 (UnaryTerm Negate t2)) = BinaryTerm Plus t1 t2
+simplify (BinaryTerm op a b) = BinaryTerm op (simplify a) (simplify b)
+
+z = UnaryTerm Negate (UnaryTerm Negate (C 5))
+z2 = simplify z
